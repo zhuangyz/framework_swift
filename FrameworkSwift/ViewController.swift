be3092deb8fafb766e8ebb2bd27259d3fb645515
zhuangyz <632647076@qq.com>
2024-11-07 17:46:11 +0800

    WZToast 0.0.1

---
+++ FrameworkSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FrameworkSwift
+//
+//  Created by linjie on 2024/11/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
